893dba551a05abeeffb243c868b5f90de54c6f40
Zino <zino@poulet.org>
2020-02-10 10:33:02 +0100

    check bounds for min/max

--- Sources/HoledRange/Transformations.swift
+++ Sources/HoledRange/Transformations.swift
@@ -7,7 +7,8 @@ extension HoledRange {
     public func transform<Q>(using f: @escaping (Bound)->Q) -> HoledRange<Q> where Q : Hashable, Q : Comparable{
         var result = HoledRange<Q>()
         for r in self.ranges {
-            let nr = ClosedRange(uncheckedBounds: (lower: f(r.lowerBound), upper: f(r.upperBound)))
+            let nbounds = [f(r.lowerBound), f(r.upperBound)].sorted()
+            let nr = ClosedRange(uncheckedBounds: (lower: nbounds[0], upper: nbounds[1]))
             result.append(nr)
         }
         for e in self.excludedValues {
